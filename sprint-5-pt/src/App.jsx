929035b238f0d134f9cb40436fa0e2b77d931c6d
Angatupyry <fierrofenix@gmail.com>
2023-04-21 09:42:45 -0400

    Day 4

--- sprint-5-pt/src/App.jsx
+++ sprint-5-pt/src/App.jsx
@@ -1,14 +1,18 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <div className="font-raleway">
       <div className="w-full bg-accentBackground">
         <Header />
+      </div>
         <main className="relative flex flex-col items-center justify-center gap-32 bg-mainBackground">
-        <Hero />
+          <Hero />
         </main>
+      <div className="w-full bg-footerBackground">
+        <Footer />
       </div>
     </div>
   )
